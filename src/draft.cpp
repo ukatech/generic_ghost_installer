f1c86dcad8ba1e18deb62c3e0217c01f84035448
steve02081504 <steve02081504@EDEN.fukwold>
2022-08-08 21:53:51 +0800

    Create draft.cpp

---
+++ src/draft.cpp
@@ -0,0 +1,15 @@
+#include "my-gists/ukagaka/SSPpath.hpp"
+int main() {
+    if(IsSSPinstalled()) {
+        //TODO download and install ghost
+    }
+    else{
+        //TODO download and install SSP
+        //get language id
+        //show install path dialog
+        //install SSP(download zip & extract)
+        //chose&install language pack & ghost for starter
+        //install ghost
+    }
+    return 0;
+}
